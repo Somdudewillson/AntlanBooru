4a67b88b51239a38d40f118f31d3c945d4b49907
Somdudewillson <somdudewilson@gmail.com>
2022-10-07 18:41:37 -0700

    Implemented artist randomization.

--- post-grid.js
+++ post-grid.js
@@ -1,4 +1,5 @@
 const aspectRatios = [[640,640],[512,768],[768,512]];
+const artists = ['Agnes Lawrence Pelton','Akihito Yoshida','Albert Bierstadt','Andy Warhol','Annibale Carracci','Anthony Van Dyck','Artstation','Asaf Hanuka','Assassin\'s Creed','Aubrey Beardsley','Ben Enwonwu','Bob Byerley','Camille Corot','Caravaggio Michelangelo Merisi','Caspar David Friedrich','Charlie Bowater','Claude Monet','David Mann','Diego Rivera','Dorothea Tanning','Edward Munch','Fernando Botero','Frank Frazetta','Frida Kahlo','Hassan Mossoudy','Hayao Miyazaki','Hazem Taha Hussein','Hieronymus Bosch','H.R. Giger','Ismail Inceoglu','Ivan Shishkin','Jeff Koons','John Constable','John Kenn Mortensen','Jose Tapiro Y Baro','Karol Bak','Katsushika Hokusai','Kiki Smith','Kim Tschang Yeul','Ko Young Hoon','Laurie Lipton','Lee Man Fong','Lemma Guya','Lisa Frank','Mahmoud Sai','Malika Agueznay','Maria Sibylla Merian','Mark Brooks','Nam June Paik','Octane render','Paolo Uccello','Pauline Haynes','Pebble Tay','Pieter Bruegel The Elder','Pre-Raphaelite Brotherhood','Raffaello Sanzio','Ralph Steadman','Remedios Varo Uranga','Rene Magritte','Richard Dadd','Roy Liechtestein','Salvador Dali','Sonia Delaunay','Takashi Murakami','Tivadar Csontváry Kosztka','Tony Diterlizzi','Victto Ngai','Wanda Gág','Wangechi Mutu','Unreal Engine','Weta Digital','Wētā FX','WLOP','Yaoy Kusama','Yoshitaka Amano','Zeng Fanzhi','Ai yazawa','Cindy Sherman','Diane Arbus','Brassaï','Francisco Goya','Man Ray','Brom','van Gogh'];
 
 const postsContainer = document.getElementsByClassName("posts-container")[0];
 
@@ -7,11 +8,29 @@ async function searchForPosts(formObject) {
     const selectedRatio = formObject["aspect-ratio"].value;
     const scale = Cookies.get('cfg_scale')!=undefined?Cookies.get('cfg_scale'):11;
     const uc = Cookies.get('user_uc')!=undefined?Cookies.get('user_uc'):'';
+    const resultCount = formObject["result-quantity"].value;
 
     const prompt = formObject["prompt"].value.trim();
     if (prompt.length <= 0) { return; }
 
-    const resultCount = formObject["result-quantity"].value;
+    // Modify prompt
+    var appendArray = Array(prompt.length).fill('');
+    const artistType = formObject["artist-injection"].value;
+    const artistAmp = formObject["artist-amp"].value;
+    switch (artistType) {
+        case 'known':
+            for (let i = 0; i < appendArray.length; i+=5) {
+                var selectedArtist = artists[Math.floor(Math.random() * artists.length)];
+                selectedArtist = ', '+"{".repeat(artistAmp)+'by '+selectedArtist+"}".repeat(artistAmp)
+                appendArray = appendArray.fill(selectedArtist,i,i+5);
+                console.log(selectedArtist)
+            }
+            break;
+        default:
+            break;
+    }
+
+    // Generate loaders
     var loaders = [];
     for (let i=0;i<resultCount;i++) {
         var newLoader = document.createElement("span");
@@ -26,13 +45,16 @@ async function searchForPosts(formObject) {
     }
 
     var seed = formObject["start-seed"].value;
+    var idx = 0;
     const shuffleRatio = selectedRatio == -1;
     for (const loader of loaders) {
         const imageSize = shuffleRatio?aspectRatios[Math.floor(Math.random() * 3)]:aspectRatios[selectedRatio];
-        const next_image_data = await fetchThumbnailImage(prompt, seed++, imageSize, scale, uc);
+        const augPrompt = prompt+appendArray[idx++];
+        const next_image_data = await fetchThumbnailImage(augPrompt, seed++, imageSize, scale, uc);
 
         var newImg = document.createElement("img");
         newImg.setAttribute("src", next_image_data.data);
+        newImg.setAttribute("alt", augPrompt);
         newImg.setAttribute('class','post-preview-image')
 
         var newLink = document.createElement("a");
